e3a67e088b9dfe016620da4590687936b83f7fa0
github username <github email address>
2021-06-13 15:35:52 -0400

    commit tree fabab207f488b99989c7c3ef96a736e999130292

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("The DOM has loaded");
+});
+
+console.log(
+  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+);
+
+const text = document.querySelector("#text");
+
+document.addEventListener("DOMContentLoaded", function () {
+  return (text.textContent = "This is really cool!");
+});
